43de8b2d8858ccb75b5c127516a9c95f55ccef99
chandresh pokiya <chandreshpokiya6574@gmail.com>
2023-02-20 12:44:24 +0530

    dbport added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,6 +3,7 @@ export default {
    HOST: process.env.HOST,
    USER: process.env.USER,
    PASSWORD: process.env.PASSWORD,
+   PORT: process.env.DB_PORT,
    DB: process.env.DB_NAME,
    dialect: 'mysql',
    pool: {
