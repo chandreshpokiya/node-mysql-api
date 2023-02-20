867686b39d53d00e36141a2a99d1e8bfbb225c65
chandresh pokiya <chandreshpokiya6574@gmail.com>
2023-02-20 12:28:49 +0530

    db name changed

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,12 +3,12 @@ export default {
    HOST: process.env.HOST,
    USER: process.env.USER,
    PASSWORD: process.env.PASSWORD,
-   DB: process.env.DB,
+   DB: process.env.DB_NAME,
    dialect: 'mysql',
    pool: {
       max: 5,
       min: 0,
-       acquire: 30000,
+      acquire: 30000,
       idle: 10000,
    },
 };
