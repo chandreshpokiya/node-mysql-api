5ea96e7b4a3cbf5b900c55f1ed040416d448727e
chandresh pokiya <chandreshpokiya6574@gmail.com>
2023-02-20 12:24:57 +0530

    config file updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,10 @@
+import 'dotenv/config'
 export default {
-   HOST: "localhost",
-   USER: "root",
-   PASSWORD: "",
-   DB: "node_sequelize_api_db",
-   dialect: "mysql",
+   HOST: process.env.HOST,
+   USER: process.env.USER,
+   PASSWORD: process.env.PASSWORD,
+   DB: process.env.DB,
+   dialect: 'mysql',
    pool: {
       max: 5,
       min: 0,
