fa0c8bf4fb217ef5bd03fdffc3591b88244c1f44
chandresh pokiya <chandreshpokiya6574@gmail.com>
2023-02-20 12:53:21 +0530

    port fixing

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -9,7 +9,6 @@ export default {
    pool: {
       max: 5,
       min: 0,
-      acquire: 30000,
       idle: 10000,
    },
 };
--- models/index.js
+++ models/index.js
@@ -6,13 +6,11 @@ import reviewModel from './reviewModel.js'
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
    host: dbConfig.HOST,
    dialect: dbConfig.dialect,
-   port: dbConfig.PORT
-   operatorsAliases: false,
+   port: dbConfig.PORT,
 
    pool: {
       max: dbConfig.pool.max,
       min: dbConfig.pool.min,
-      acquire: dbConfig.pool.acquire,
       idle: dbConfig.pool.idle,
    },
 });
