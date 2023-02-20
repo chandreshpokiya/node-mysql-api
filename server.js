aa90c647310ab298993b3424b733a380fa9dbfd8
chandresh pokiya <chandreshpokiya6574@gmail.com>
2023-02-20 12:40:37 +0530

    testing apis

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-import express from 'express';
-import cors from 'cors'
-import productRouter from './routes/product.js'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import productRouter from "./routes/product.js";
+import "dotenv/config";
 
-const port = process.env.PORT ||5001
+const port = process.env.PORT || 5001;
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/products', productRouter)
+app.get("/", (req, res) => {
+   res.json("yup it's working");
+});
+app.use("/api/products", productRouter);
 
-app.listen(port, err => {
-    err ? console.log(err) : console.log('server is up and running on port ' + port)
-})
+app.listen(port, (err) => {
+   err ? console.log(err) : console.log("server is up and running on port " + port);
+});
